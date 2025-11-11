b30e93ca23950ef2ab9a2362282ec58d5c0360ab
Guilherme Mendes <guilhermemendes.info@gmail.com>
2025-11-11 20:09:06 -0300

    manipulando arrays

---
+++ ManipulandoArray.php
@@ -0,0 +1,16 @@
+<?php
+
+header('content-type: text/html; charsert=utf8');
+
+$Arr = array("php", "html", "css");
+var_dump($Arr);
+echo "<hr>";
+$Arrb = ["php", "html", "css"];
+var_dump($Arrb);
+
+/**array associativo */
+echo "<hr>";
+$arrb = ["casa" => "carpina", "dono" => "guilheme", "esposa" => "Nanda"];
+
+var_dump($arrb);
+
